a2fcf8274b5c270e929f94571bc6b501d081066f
muricawashere <csgo4life69@gmail.com>
2018-09-11 21:20:56 -0700

    upload

--- start.js
+++ start.js
@@ -127,15 +127,16 @@ ${data.body.tracks.items.map(song => `**${++index}**. **${song.name}** - **${son
 \n
 __If you want to play a song respond with your selection__`)
                 try {
-                    var selection = msg.channel.awaitMessages(remsg => remsg.content > 0 && remsg < 11, {
+                    msg.channel.awaitMessages(remsg => remsg.content > 0 && remsg < 11, {
                         maxMatches: 1,
                         time: 10000,
                         errors: ['time']
-                    })
+                    }).then((res) => {
+                        console.log(res)
+                    }).catch(err => console.error)
                 } catch(err) {
                     return
                 }
-                console.log(selection)
             }, err => console.error(err))
         })
     }
